121c8e72630be533992de4eb757b1ea592be4175
karl-aleksander <KarlTheFish@users.noreply.github.com>
2023-09-15 13:51:26 +0300

    Progress on API call

--- Models/TravelRouteModel.cs
+++ Models/TravelRouteModel.cs
@@ -1,13 +1,9 @@
 namespace CosmosOdyssey.Models;
 
 public class TravelRouteModel {
-    public static int ID { get; set; }
-    public static string From { get; set; }
-    public static string To { get; set; }
-    public static int Distance { get; set; }
-    public static List<Provider> Providers { get; set; }
-
-    public TravelRouteModel() {
-        ID = -1;
-    }
+    public string ID { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public int Distance { get; set; }
+    public List<Provider> Providers { get; set; }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -12,7 +12,7 @@ namespace CosmosOdyssey.Pages
         private HttpClient _httpClient;
 
         [BindProperty]
-        public static TravelRouteModel? TravelRoute { get; set; }
+        public TravelRouteModel TravelRoute { get; set; }
         
         [BindProperty(SupportsGet = true)]
         public string fromSelection { get; set; }
@@ -23,75 +23,40 @@ namespace CosmosOdyssey.Pages
             fromSelection = Request.Query["from"];
             toSelection = Request.Query["to"];
             if (fromSelection != toSelection) {
-                TravelSubmitted(fromSelection, toSelection);
+                await TravelSubmitted(fromSelection, toSelection);
             }
             return Page();
         }
 
-        public IActionResult TravelSubmitted(string from, string to) {
+        public async Task<IActionResult> TravelSubmitted(string from, string to) {
+            await TravelSubmitHandler(from, to);
             return RedirectToPage("index", new { from = from, to = to });
         }
         
-        public async Task<IActionResult> TravelSubmitHandler() {
-            Console.WriteLine("TravelSubmitHandler called!");
+        public async Task<IActionResult> TravelSubmitHandler(string from, string to) {
             // Initialize with default values
             _httpClient = new HttpClient();
             TravelRoute = new TravelRouteModel();
-
-            string from = Request.Query["from"];
-            string to = Request.Query["to"];
             
             // Make an API request to get route information based on the selected planets
             string apiUrl = $"https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices/?from={from}&to={to}"; // Replace with the actual API URL
             HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+            Console.WriteLine(apiUrl);
 
             if (response.IsSuccessStatusCode)
             {
+                Console.WriteLine("API call success");
                 string apiResponseJson = await response.Content.ReadAsStringAsync();
-
+                
                 // Deserialize the JSON response manually
                 TravelRoute = JsonConvert.DeserializeObject<TravelRouteModel>(apiResponseJson);
-
+                
                 return Page();
             }
             else
             {
                 // Handle API request failure
                 Console.WriteLine("API error?");
-                return Page(); // You can customize this to display an error message
-            }
-        }
-
-        public IActionResult FindRouteModel()
-        {
-            Console.WriteLine("FindRouteModel called!");
-            _httpClient = new HttpClient();
-            TravelRoute = new TravelRouteModel();
-            return Page();
-        }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            Console.WriteLine("OnPostAsync called!");
-            var from = Request.Form["from"];
-            var to = Request.Form["to"];
-
-            // Make an API request to get route information based on the selected planets
-            string apiUrl = $"https://cosmos-odyssey.azurewebsites.net/api/v1.0/TravelPrices/?from={from}&to={to}"; // Replace with the actual API URL
-            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
-
-            if (response.IsSuccessStatusCode)
-            {
-                string apiResponseJson = await response.Content.ReadAsStringAsync();
-
-                // Deserialize the JSON response manually
-                TravelRoute = JsonConvert.DeserializeObject<TravelRouteModel>(apiResponseJson);
-
-                return Page();
-            }
-            else
-            {
-                // Handle API request failure
                 return Page(); // You can customize this to display an error message
             }
         }
